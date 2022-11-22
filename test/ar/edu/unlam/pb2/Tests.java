76b8528803048b758a8fe85fa28d61eeb9e479e7
LudmiAbril <ludmila.pereyra543@gmail.com>
2022-11-21 22:44:36 -0300

    test 6 y7

--- test/ar/edu/unlam/pb2/Tests.java
+++ test/ar/edu/unlam/pb2/Tests.java
@@ -2,6 +2,9 @@ package ar.edu.unlam.pb2;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 public class Tests {
@@ -56,6 +59,44 @@ public class Tests {
 		
 		assertEquals(esperado,obtenido);
 		
+	}
+	
+	@Test
+	public void queLasFiguritasAgregadasDeFormaDesordenadaQuedenOrdenadas() throws CodigoExistenteException {
+		Usuario admin=new UsuarioAdministrador("admin");
+		Sistema sistema=new Sistema("sistema-panini");
+		Figurita figu1=new Figurita("ARG23", 'A', "Argentina", 23,"Messi", 100.0);
+		Figurita figu2=new Figurita("ARG24", 'B', "Argentina", 24,"Dibu", 100.0);
+		Figurita figu3=new Figurita("ARG25", 'C', "Argentina", 25,"De Paul", 100.0);
+		((UsuarioAdministrador)admin).agregarFigurita(figu1, sistema);
+		((UsuarioAdministrador)admin).agregarFigurita(figu2, sistema);
+		((UsuarioAdministrador)admin).agregarFigurita(figu3, sistema);
+		
+		Usuario usfinal=new UsuarioFinal("final");
+		((UsuarioFinal)usfinal).agregarFigurita(figu1, sistema);
+		((UsuarioFinal)usfinal).agregarFigurita(figu2, sistema);
+		((UsuarioFinal)usfinal).agregarFigurita(figu3, sistema);
+		
+		List<Figurita>listaOrdenada=new ArrayList<Figurita>();
+		listaOrdenada.add(figu1);
+		listaOrdenada.add(figu2);
+		listaOrdenada.add(figu3);
+		
+		List<Figurita>listaObtenida=((UsuarioFinal)usfinal).getStockFiguritas();
+		
+		assertEquals(listaOrdenada,listaObtenida);
+		
+	}
+	
+	@Test(expected = CodigoExistenteException.class)
+	public void queUnAdministradorNoPuedaAgregarUnaFiguritaExistente() throws CodigoExistenteException {
+		Usuario admin=new UsuarioAdministrador("admin");
+		Sistema sistema=new Sistema("sistema-panini");
+		Figurita figu=new Figurita("ARG23", 'C', "Argentina", 1,"Messi", 100.0);
+		
+		((UsuarioAdministrador)admin).agregarFigurita(figu, sistema);
+		((UsuarioAdministrador)admin).agregarFigurita(figu, sistema);
+			
 	}
 	
 	
